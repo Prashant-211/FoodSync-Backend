6397745a2859b0b2b875099435f7a0d4e1c3052b
Prashant <mprashant9451@gmail.com>
2024-12-24 20:54:06 +0530

    Tested userschema

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const ServerConfig = require('./config/serverConfig')
 const connectDB = require('./config/dbConfig')
+// const User = require('./schema/userSchema')
 
 const app = express();
 
@@ -17,4 +18,13 @@ app.listen(ServerConfig.PORT,async () => {
     await connectDB();
     console.log(`Server started at port ${ServerConfig.PORT}...`);
 
+    // const newUser = await User.create({
+    //     email:'abc@b.com',
+    //     password: '123456',
+    //     firstName: 'Jonathan',
+    //     lastName:'Majoras',
+    //     mobileNumber:'7783123128'
+    //});
+    // console.log("Created new user");
+    // console.log(newUser)
 })
