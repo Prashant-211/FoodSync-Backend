bf98e8d244d4a17b368a01e24f959b1ac5336749
Prashant <mprashant9451@gmail.com>
2025-01-06 18:48:15 +0530

    Added isLoggedIn middleware validator

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser')
 const ServerConfig = require('./config/serverConfig')
 const connectDB = require('./config/dbConfig');
 const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
+const { isLoggedIn } = require('./validation/authValidator');
 // const User = require('./schema/userSchema')
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended:true}));
 
 // Routing middleware
 // if your req route starts with /users then handle it using userRouter
 app.use('/users',userRouter); // connects the router to the server
 app.use('/carts', cartRouter);
-app.use('/auth', authRouter)
+app.use('/auth', authRouter);
+
+app.get('/ping',isLoggedIn,(req,res) => {
+    //controller
+    console.log(req.body);
+    console.log(req.cookies);
+    return res.json({message: "pong"})
+})
 
 app.listen(ServerConfig.PORT,async () => {
     await connectDB();
