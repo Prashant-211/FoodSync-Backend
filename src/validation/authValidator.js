481bb1ce0a0231dfd659a68bfd0e6a6b89ff729e
Prashant <mprashant9451@gmail.com>
2025-01-23 12:20:46 +0530

    Added admin role checks for authorization

--- src/validation/authValidator.js
+++ src/validation/authValidator.js
@@ -12,27 +12,55 @@ async function isLoggedIn(req, res, next) {
         });
     }
 
-    const decoded = jwt.verify(token, JWT_SECRET);
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
 
-    if(!decoded) {
+        if(!decoded) {
+            throw new UnAuthorisedError();
+        }
+        // if reached here, then user is authenticated allow them to access the api
+
+        req.user = {
+            email: decoded.email,
+            id: decoded.id,
+            role: decoded.role
+        }
+
+        next();
+    } catch(error){
         return res.status(401).json({
             success: false,
             data: {},
-            error: "Not authenticated",
+            error: error,
             message: "Invalid Token provided"
         });
     }
-
-    // if reached here, then user is authenticated allow them to access the api
-
-    req.user = {
-        email: decoded.email,
-        id: decoded.id
-    }
-
-    next();
 }
 
+/**
+ * This function checks if the authenticated user is an admin or not ?
+ * Becuase we will call isAdmin after isLoggedIn thats why we will receive user details
+ */
+function isAdmin(req, res, next) {
+    const loggedInUser = req.user;
+    console.log(loggedInUser);
+    if(loggedInUser.role === "ADMIN") {
+        console.log("User is an admin");
+        next();
+    } else{
+        return res.status(401).json({
+            success: false,
+            data:{},
+            message: "You are not authorised for this action",
+            error: {
+                statusCode: 401,
+                reason: "Unauthorised user for this action"
+            }
+        })
+    }
+    
+}
 module.exports = {
-    isLoggedIn
+    isLoggedIn,
+    isAdmin
 }
