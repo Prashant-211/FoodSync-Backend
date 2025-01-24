3ec111e9ccf2c299b1739240dbda36908efb470c
Prashant <mprashant9451@gmail.com>
2025-01-24 15:14:17 +0530

    Added clear cart functionality

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCartByUser, modifyProductTocart } = require('../controllers/cartController');
+const { getCartByUser, modifyProductTocart, clearCartbyId } = require('../controllers/cartController');
 const { isLoggedIn } = require('../validation/authValidator');
 
 const cartRouter = express.Router();
@@ -7,5 +7,8 @@ const cartRouter = express.Router();
 cartRouter.get('/',isLoggedIn, getCartByUser);
 
 cartRouter.post('/:operation/:productId',isLoggedIn,modifyProductTocart)
+
+
+cartRouter.delete('/products', isLoggedIn, clearCartbyId);
 
 module.exports = cartRouter;
