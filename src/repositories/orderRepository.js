81c395d2d71c84f436b71100c256352b5defe746
Prashant <mprashant9451@gmail.com>
2025-01-26 13:52:16 +0530

    Added order creation api

---
+++ src/repositories/orderRepository.js
@@ -0,0 +1,23 @@
+const Order = require("../schema/orderSchema");
+const InternalServerError = require("../utils/internalServerError");
+
+async function createNewOrder(orderDetails) {
+    try {
+        const order = await Order.create(orderDetails);
+        return order;
+    } catch(error) {
+        if(error.name === 'ValidationError') {
+
+            const errorMessageList = Object.keys(error.errors).map((property) => {
+                return error.errors[property].message;
+            })
+            throw new BadRequestError(errorMessageList);
+        } 
+        console.log(error);
+        throw new InternalServerError();
+    }
+}
+
+module.exports = {
+    createNewOrder
+}
